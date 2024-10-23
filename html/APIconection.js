a0ddf40d0a229e437d53f34f38f36fd397f326e9
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2024-10-22 19:52:38 -0500

    😢
    la cosa se esta poniendo maluca, en que punto la cague

--- html/APIconection.js
+++ html/APIconection.js
@@ -1,22 +1,24 @@
+
 async function obtenerAves() {
     try {
         const response = await fetch('http://localhost:8080/API/aves');
         const data = await response.json()
-        const birdContainer = document.getElementById('bird-container')
+        const avesContainer = document.getElementById('aves-container')
 
-        data.results.forEach(bird => {
-            const birdElement = document.createElement('div')
-            birdElement.classList.add('bird')
-            birdElement.innerHTML = `<img id=imagen src="${bird.image}" alt="{bird.name}">
+        data.results.forEach(aves => {
+            const avesElement = document.createElement('div')
+            avesElement.classList.add('aves')
+            avesElement.innerHTML = `<img id=imagen src="${aves.imgUrl}" alt="{aves.nombre}">
         <div>
-            <h3 id="n">NOMBRE: ${bird.name}</h1>
-            <p>NOMBRE: ${bird.nombre}</p>
-            <p>ALIMENTACIÓN: ${bird.alimentación}</p>
+            <h3>NOMBRE: ${aves.nombre}</h1>
+            <p>NOMBRE: ${aves.nombre}</p>
+            <p>ALIMENTACIÓN: ${aves.alimentación}</p>
         </div>`
-        birdContainer.appendChild(birdElement)
+        avesContainer.appendChild(avesElement)
         });
     } catch (error) {
         console.error('Error: no se puedo obtener el mensaje', error);
+
     }
 }
 window.onload = obtenerAves
