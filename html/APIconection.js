a5af4fc8c97ae5691b315cfce63edc048cc12de4
D-A-V-I-D-A-R-A-N-G-O <davidpeewee01241@outlook.com>
2024-10-22 21:23:17 -0500

    listo
    me di cuenta de lo pendejo que soy y de qeu no se adaptar el codigo, por ponerme a inventar, uno de los objetos en el json tenia varios(era array creo) y el de rick and morty no tenia eso tonces no funcionava de manera correcta, agregando qeu el mio no tenia results por lo cual no lo identificava en el json, lo borre y salio pa pintura.
    Ahora solo qeuda un poco de css y agregar más aves

--- html/APIconection.js
+++ html/APIconection.js
@@ -1,20 +1,19 @@
-
 async function obtenerAves() {
     try {
         const response = await fetch('http://localhost:8080/API/aves');
         const data = await response.json()
         const avesContainer = document.getElementById('aves-container')
         console.log(data)
-        data.results.forEach(ave => {
-            const avesElement = document.createElement('div')
-            avesElement.classList.add('ave')
-            avesElement.innerHTML = `<img id=imgUrlS src="${ave.imgUrl}" alt="${ave.nombre}">
+        data.forEach(ave => {
+            const aveElement = document.createElement('div')
+            aveElement.classList.add('ave')
+            aveElement.innerHTML = `<img id=imgUrlS src="${ave.imgUrl}" alt="${ave.nombre}">
         <div>
             <h3>NOMBRE: ${ave.nombre}</h1>
             <p>NOMBRE: ${ave.nombre}</p>
             <p>ALIMENTACIÓN: ${ave.alimentación}</p>
         </div>`
-        avesContainer.appendChild(avesElement)
+        avesContainer.appendChild(aveElement)
         });
     } catch (error) {
         console.error("no se pudo hacer", error)
